2128ca7a8019bd3653ac45cb677abdfa179b7269
mustafaerrgun <mustafa.erguen@tum.de>
2025-11-24 13:49:30 +0300

    Add XDR RCC driver
    - Implement RCC initialization and configuration functions
    - Add private APIs for clock source and bus clock configuration
    - Include flash latency handling wait states
    - Provide system clock retrieval functionality
    - Test RCC driver module for 48MHz system clock
    - Configure RCC handle with PLL clock source and prescalers
    - Initialize RCC and retrieve system clock frequency
    - Update README

---
+++ Src/SystemClock_Test.c
@@ -0,0 +1,39 @@
+/**
+ ******************************************************************************
+ * @file           : SystemClock_Test.c
+ * @brief          : Test XDR RCC Driver module for 48MHz System Clock
+ ******************************************************************************
+ */
+
+#include <stdint.h>
+#include "stm32f767xx_xdr_rcc.h"
+
+int main(void)
+{
+    XDR_RCC_Handle testHandle;
+    testHandle.XDR_RCC = RCC;
+    testHandle.XDR_RCC_Config.ClockSource = XDR_PLL_CLOCK;
+    testHandle.XDR_RCC_Config.SYSCLK_Freq = XDR_SYSCLK_48MHZ;
+    testHandle.XDR_RCC_Config.AHB_Prescaler = XDR_AHB_DIV4;
+    testHandle.XDR_RCC_Config.APB1_Prescaler = XDR_APB_DIV2;
+    testHandle.XDR_RCC_Config.APB2_Prescaler = XDR_APB_DIV2;
+
+
+
+    XDR_RCC_Init(&testHandle);
+
+    volatile uint32_t system_clock = XDR_Get_SysClock(&testHandle);
+
+
+	while(1){
+
+
+	}
+	return 0 ;
+}
+
+
+
+
+
+
